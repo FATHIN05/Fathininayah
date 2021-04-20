7edd6fa6640e4fbf2de6c09cb8e4c8b708375c61
FATHIN05 <fathininayah270@gmail.com>
2021-04-20 12:41:30 +0700

    Initial sharing of project

---
+++ Main.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class Main here.
+ *
+ * @author (Fathin inayah)
+ * @version (a version number or a date)
+ */
+public class Main
+{
+    public static void main(String args[]){
+     // menciptakan objek baru dari class Calculator
+    kalkulator kalkul = new kalkulator();
+    
+    
+    kalkul.name = "Operator ";
+    
+     //author
+    System.out.println("---OPERATOR DASAR PADA JAVA ---");
+     //inputan oprator yg dijalankan
+    System.out.println("nilai 1 = "+ kalkul.a);
+    System.out.println("nilai 2 = "+ kalkul.b);
+     //memanggil atribut hasil
+    System.out.println(kalkul.name + "penjumlahan 4 + 5 ="+ kalkul.penjumlahan);
+    System.out.println(kalkul.name + "pengurangan 4 - 5 ="+ kalkul.pengurangan);
+    System.out.println(kalkul.name + "perkalian 4 * 5 ="+ kalkul.perkalian);
+    System.out.println(kalkul.name + "pembagian 4 / 5 ="+ kalkul.pembagian);
+}
+}    
+    
+    
+    
+
+    
---
+++ kalkulator.java
@@ -0,0 +1,24 @@
+
+/**
+ * Write a description of class kalkulator here.
+ *
+ * @author(Fathin inayah)
+ * @version (a version number or a date)
+ */
+public class kalkulator
+{
+    String name;
+    
+    int a = 4;
+    int b = 5;
+    
+    
+    int penjumlahan = a + b;
+    int perkalian = a * b;
+    int pengurangan = a - b;
+    int pembagian = a / b;
+    
+}  
+    
+    
+        
