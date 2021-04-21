883fab9e14ff5df922832256a775ab28debfe185
FATHIN05 <fathininayah270@gmail.com>
2021-04-21 13:29:51 +0700

    main

--- Main.java
+++ Main.java
@@ -3,29 +3,44 @@
  * Write a description of class Main here.
  *
  * @author (Fathin inayah)
- * @version (a version number or a date)
+ * @version 0.1 up
  */
-public class Main
-{
-    public static void main(String args[]){
-     // menciptakan objek baru dari class Calculator
-    kalkulator kalkul = new kalkulator();
+public class Main{
+   
+        public static void main (String args[]){
+           
+            kalkulator kalkul = new kalkulator();
+            
+            
+            System.out.println("");
+       double hasil1 = kalkul.jumlah();
+       double hasil2 = kalkul.jumlah(3.4, 5.4);
+       System.out.println("Hasil penjumlahan method jumlah tanpa parameter: "+ hasil1);
+       System.out.println("Hasil penjumlahan method jumlah dengan parameter: "+ hasil2);
+
+       System.out.println("");
+       double hasil3 = kalkul.kurang();
+       double hasil4 = kalkul.kurang(10, 5);
+       System.out.println("Hasil perkurangan method kurang tanpa parameter: "+ hasil3);
+       System.out.println("Hasil perkurangan method kurang dengan parameter: "+ hasil4);
+    
+       System.out.println("");
+       double hasil5 = kalkul.kali();
+       double hasil6 = kalkul.kali(3, 5);
+       System.out.println("Hasil perkalian method kali tanpa parameter: "+ hasil5);
+       System.out.println("Hasil perkalian method kali dengan parameter: "+ hasil6);
+
+       System.out.println("");
+       double hasil7 = kalkul.bagi();
+       double hasil8 = kalkul.bagi(10, 2);
+       System.out.println("Hasil Pembagian method bagi tanpa parameter: "+ hasil7);
+       System.out.println("Hasil Pembagian method bagi dengan parameter: "+ hasil8);
     
     
-    kalkul.name = "Operator ";
-    
-     //author
-    System.out.println("---OPERATOR DASAR PADA JAVA ---");
-     //inputan oprator yg dijalankan
-    System.out.println("nilai 1 = "+ kalkul.a);
-    System.out.println("nilai 2 = "+ kalkul.b);
-     //memanggil atribut hasil
-    System.out.println(kalkul.name + "penjumlahan 4 + 5 ="+ kalkul.penjumlahan);
-    System.out.println(kalkul.name + "pengurangan 4 - 5 ="+ kalkul.pengurangan);
-    System.out.println(kalkul.name + "perkalian 4 * 5 ="+ kalkul.perkalian);
-    System.out.println(kalkul.name + "pembagian 4 / 5 ="+ kalkul.pembagian);
+   }
 }
-}    
+   
+     
     
     
     
